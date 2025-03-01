89ba1c8f7323ea82df9b536271e22c4eed3c928d
demerson05 <demersondamascena18@gmail.com>
2025-03-01 09:02:52 -0300

    new

---
+++ Quest-Cap8/1.js
@@ -0,0 +1,15 @@
+/*
+
+Reutilização de código
+A modularização permite reaproveitar partes do código em diferentes projetos ou áreas do mesmo sistema,
+economizando tempo e esforço. Por exemplo, um módulo de autenticação pode ser usado tanto em um aplicativo web quanto em uma API móvel.
+
+Facilidade de manutenção
+Dividir o código em módulos menores facilita a identificação e correção de erros, além de permitir alterações sem impactar o sistema inteiro.
+Por exemplo, ao corrigir um problema no módulo de envio de e-mails, não é necessário revisar outros módulos.
+
+Colaboração eficiente
+Com módulos independentes, equipes podem trabalhar simultaneamente em diferentes partes do projeto, aumentando a produtividade.
+Por exemplo, uma equipe pode desenvolver o módulo de pagamentos enquanto outra cuida do módulo de relatórios.
+
+*/
